89ba02f301d1e431265f8ad63424a9a280a8e6f5
gabriel-java-github <gabriel.haberlach@gmail.com>
2022-04-06 11:18:53 +0200

    Initial Commit

---
+++ script.js
@@ -0,0 +1,54 @@
+// Ein Großteil des Scripts habe ich von: https://www.w3schools.com/howto/howto_js_tabs.asp
+
+function onLoadFunctions(){
+    document.getElementById("codeBtn").focus();
+}
+window.onload = onLoadFunctions;
+
+function openCode() {
+
+    document.getElementById("codeBtn").focus();
+
+    // Declare all variables
+    var i, tabcontent, tablinks;
+
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById("Code").style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+function openConsole() {
+
+    document.getElementById("consoleBtn").focus();
+
+    // Declare all variables
+    var i, tabcontent, tablinks;
+
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById("Console").style.display = "block";
+    evt.currentTarget.className += " active";
+}
